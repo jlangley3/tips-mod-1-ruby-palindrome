592d6d746c559f09a7670df12a37bdbf1d7eef60
jlangley3 <jlangleyiii@aol.com>
2020-05-06 09:53:11 -0400

    done first

--- solution.rb
+++ solution.rb
@@ -1,1 +1,12 @@
+def palindrome?(word)#spelled the same foward and backward (race car)
+    rev = []
+    arr = word.split("")
+    arr.each do |letter|
+        rev.unshift(letter)
+    end
+    return rev.join("") == word
+end
 
+puts palindrome?("noon")
+puts palindrome?("racecar")
+puts palindrome?("place of business")
